80a3389b59b8a8dfb79fc3440016f1a6c2030e7c
ashah <ashah@10.0.0.172>
2023-03-17 06:57:29 -0400

    My Second Commit

---
+++ src/Demo/Repo.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class Repo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String young = "Yes";
+		if (young == "Yes") {
+			System.out.println("Your age is Under 20");
+		}
+		else {
+			System.out.println("You are Old");
+		}
+
+	}
+
+}
